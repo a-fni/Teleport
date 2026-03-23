0d4666d94073148f0edb0756da8e2e213cf7052d
a-fni <andreaferrarini.dev@gmail.com>
2026-03-23 14:49:30 +0100

    Improve file and directory structure of teleporters

--- teleport.sh
+++ teleport.sh
@@ -7,15 +7,42 @@ set -eou pipefail
 source teleporter.conf
 
 # Check if local teleporter exists
-if [[ ! -d "$LOCAL_TELEPORTER_PATH/teleporter" ]]; then
+if [[ ! -d "$TELEPORTER_PATH/$TELEPORTER_NAME" ]]; then
   echo "Local teleporter does not exist, nothing to teleport."
   echo "Creating an empty one for next teleportation"
-  mkdir "$LOCAL_TELEPORTER_PATH/teleporter"
+  mkdir "$TELEPORTER_PATH/$TELEPORTER_NAME"
   exit 1
 fi
 
 # Usage string for documentation purtposes
 USAGE="./teleport.sh away|here|copy|paste [clone]"
+
+
+# Define generic clipboard operation to ensure multiplaform
+clipcopy() {
+  if command -v wl-copy &>/dev/null; then
+    wl-copy "$@"
+  elif command -v xclip &>/dev/null; then
+    xclip -selection clipboard "$@"
+  elif command -v pbcopy &>/dev/null; then  # macOS
+    pbcopy "$@"
+  else
+    echo "No clipboard tool found" >&2
+  fi
+}
+
+clippaste() {
+  if command -v wl-paste &>/dev/null; then
+    wl-paste --no-newline "$@"
+  elif command -v xclip &>/dev/null; then
+    xclip -selection clipboard -o "$@"
+  elif command -v pbpaste &>/dev/null; then  # macOS
+    pbpaste "$@"
+  else
+    echo "No clipboard tool found" >&2
+  fi
+}
+
 
 echo "Connecting to remote portal..."
 
@@ -29,27 +56,30 @@ elif [[ "$#" == 2 && "$2" != "clone" ]]; then
   echo "Unknown argument $2 passed."
   echo "$USAGE"
 elif [[ "$1" == "away" ]]; then
-  scp -r "$LOCAL_TELEPORTER_PATH/teleporter"/* "$REMOTE_MACHINE:$REMOTE_TELEPORTER_PATH/.teleporter"
+  scp -r "$TELEPORTER_PATH/$TELEPORTER_NAME"/* "$REMOTE_MACHINE:$TELEPORTER_PATH/$TELEPORTER_NAME"/
   if [[ "$#" == 1 ]]; then
-    mkdir -p "$LOCAL_TELEPORTER_PATH/.teleported"
-    rm -rf "$LOCAL_TELEPORTER_PATH/.teleported"/*
-    mv "$LOCAL_TELEPORTER_PATH/teleporter"/* "$LOCAL_TELEPORTER_PATH/.teleported"/
+    rm -rf "$TELEPORTER_PATH/$TELEPORTER_NAME"/*
+    #mkdir -p "$TELEPORTER_PATH/.teleported"
+    #rm -rf "$TELEPORTER_PATH/.teleported"/*
+    #mv "$TELEPORTER_PATH/$TELEPORTER_NAME"/* "$TELEPORTER_PATH/.teleported"/
     echo "Teleportation completed!"
   elif [[ "$2" == "clone" ]]; then
     echo "Clonation completed!"
   fi
 elif [[ "$1" == "here" ]]; then
-  scp -r "$REMOTE_MACHINE:$REMOTE_TELEPORTER_PATH/.teleporter"/* "$LOCAL_TELEPORTER_PATH/teleporter/"
+  scp -r "$REMOTE_MACHINE:$TELEPORTER_PATH/$TELEPORTER_NAME"/* "$TELEPORTER_PATH/$TELEPORTER_NAME"/
   if [[ "$#" == 1 ]]; then
-    ssh "$REMOTE_MACHINE" "rm -rf $REMOTE_TELEPORTER_PATH/.teleporter/*"
+    ssh "$REMOTE_MACHINE" "rm -rf $TELEPORTER_PATH/$TELEPORTER_NAME/*"
     echo "Teleportation completed!"
   elif [[ "$2" == "clone" ]]; then
     echo "Clonation completed!"
   fi
 elif [[ "$1" == "copy" ]]; then
-  echo "Copy"
+  scp "$REMOTE_MACHINE:$CLIPBOARD_PATH/$CLIPBOARD_NAME" "$CLIPBOARD_PATH/$CLIPBOARD_NAME"
+  cat "$CLIPBOARD_PATH/$CLIPBOARD_NAME" | clipcopy
 elif [[ "$1" == "paste" ]]; then
-  echo "paste"
+  clippaste > "$CLIPBOARD_PATH/$CLIPBOARD_NAME"
+  scp "$CLIPBOARD_PATH/$CLIPBOARD_NAME" "$REMOTE_MACHINE:$CLIPBOARD_PATH/$CLIPBOARD_NAME"
 else
   echo "Unknown argument $1 passed."
   echo "$USAGE"
